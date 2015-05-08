4e92927f3c3f87c916fe6e86d66b770ca51a8a8b
Alex Beznos <beznosa@yahoo.com>
2015-05-08 18:31:18 +0300

    scheduller fix

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -1,16 +1,18 @@
 require 'rufus-scheduler'
 
-scheduler = Rufus::Scheduler.new
+scheduler = Rufus::Scheduler.singleton
 
-scheduler.every '15m' do
+scheduler.every '1m' do
   Rails.logger.info 'Scheduler is functioning'
 end
 
 scheduler.every '3h' do
+  Rails.logger.info 'Unneeded schedulers deleting'
   Schedule.where("created_at < ? and user_id = ?", 2.hours.ago, nil).destroy_all
 end
 
 scheduler.cron("0 9 * * *") do
+  Rails.logger.info 'Assignments start sending'
   sender = AssignmentSender.new
   sender.find_assignments
   sender.notify
