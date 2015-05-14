379727c1fec7b3a98b2cac8e4cc1e09fbd20709a
Alex Beznos <beznosa@yahoo.com>
2015-05-14 13:44:18 +0300

    fix assignments creation algorithm

--- app/models/concerns/assignment_creation_helper.rb
+++ app/models/concerns/assignment_creation_helper.rb
@@ -10,103 +10,60 @@ module AssignmentCreationHelper
       end
     end
 
-    self.toggle
+    self.update(active: true)
   end
 
   def create_regular_assignments(sum)
     div = sum / self.duration
-    mod = sum % self.duration
-    names = []
+    current_div = 0
+    rest_books = []
+    arr = []
+    mod = 0
 
-    self.gen_assignments_with_mod(0, div, mod, 0, first: true)
-    self.toggle
-  end
+    self.books.each_with_index do |book, index|
+      if mod == 0
+        number_of_times = book.parts_number / div
 
-  def gen_assignments_with_mod(book_num, div, mod, day_from, **prev)
-    book = self.books[book_num]
-    if book
-      sum = div + mod
-      with_mod = false
+        number_of_times.times do |time|
+          arr.push("#{book.name} #{time == 0 ? '1 - ' + div.to_s : ((time * div) + 1).to_s + ' - ' + ((time * div) + div).to_s}")
+        end
 
-      if mod != 0
-        if book.parts_number > sum
-          if prev[:first]
-            Assignment.create(name: "#{book.name} 1 - #{sum}",
-                              sending_date: self.start_date + day_from,
-                              schedule_id: self.id)
-          else
-            Assignment.create(name: "#{prev[:book_name]} #{prev[:part]} - #{book.name} #{sum}",
-                              sending_date: self.start_date + day_from,
-                              schedule_id: self.id)
-          end
+        mod = book.parts_number % div
+      else
+        if rest_books.empty?
+          current_div = 0
+          b = self.books[index - 1]
+          rest_books.push("#{b.name} #{b.parts_number - mod + 1} - #{b.parts_number}")
+        end
 
-          day_from += 1
-          with_mod = true
+        current_div == 0 ? current_div = div - mod : current_div = mod
+
+        if book.parts_number < current_div
+          rest_books.push("#{book.name} 1 - #{book.parts_number}")
+          mod = current_div - book.parts_number
         else
-          if prev[:first]
-            hash = {:book_name => book.name, :part => 1}
-          else
-            hash = prev
+          rest_books.push("#{book.name} 1 - #{current_div}")
+          arr.push(rest_books.join(', '))
+          rest_books = []
+
+          ((book.parts_number - current_div) / div).times do |time|
+            arr.push("#{book.name} #{time == 0 ? (current_div + 1).to_s + ' - ' + (div + current_div).to_s : ((time * div) + 1 + mod).to_s + ' - ' + ((time * div) + div + mod).to_s}")
           end
-          return self.gen_assignments_with_mod(book_num + 1, div, mod - book.parts_number, day_from, hash )
+
+          mod = (book.parts_number - current_div) % div
         end
       end
-
-
-      if with_mod
-        names_array = self.gen_names_array( book: book,
-                                            number_of_times: ((book.parts_number - sum) / div),
-                                            div: div,
-                                            mod: mod,
-                                            part: sum)
-        new_mod = (book.parts_number - sum) % div
-      else
-        names_array = self.gen_names_array( book: book,
-                                            number_of_times: ((book.parts_number / div) - 1),
-                                            div: div,
-                                            part: sum)
-        new_mod = book.parts_number % div
-      end
-
-      new_day_from = self.create_assignments_from_array(names_array[:arr], day_from)
-
-
-      hash = {}
-      unless new_mod == 0
-        hash =  {:book_name => book.name, :part => names_array[:till]}
-      end
-
-      self.gen_assignments_with_mod(book_num + 1, div, new_mod, new_day_from, hash)
     end
+
+    self.create_assignments_from_array(arr)
+    self.update(active: true)
   end
 
-  def create_assignments_from_array(arr, day_from)
-    arr.each do |name|
-      Assignment.create(name: name, sending_date: self.start_date + day_from, schedule_id: self.id)
-      day_from += 1
+  def create_assignments_from_array(arr)
+    arr.each_with_index do |name, index|
+      Assignment.create(name: name,
+                        sending_date: self.start_date + index,
+                        schedule_id: self.id)
     end
-
-    day_from
-  end
-
-  def gen_names_array(**hash)
-    arr = []
-
-    till = 0
-    hash[:number_of_times].times do |time|
-      real_time = time + 1
-
-      if hash[:mod]
-        from = real_time == 1 ? (hash[:div] + hash[:mod]) : (real_time * hash[:div]) + hash[:mod]
-      else
-        from = real_time == 1 ? 1 : real_time * hash[:div]
-      end
-
-      till = from + hash[:div]
-      arr.push("#{hash[:book].name} #{ from } - #{ till }")
-    end
-
-    till = hash[:part] if till == 0
-    {:arr => arr, :till => till}
   end
 end
