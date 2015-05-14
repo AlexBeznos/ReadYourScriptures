5cb3f20e591fe288ddf337c6f71f9435dd016a98
Alex Beznos <beznosa@yahoo.com>
2015-05-15 00:12:49 +0300

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150512191438) do
+ActiveRecord::Schema.define(version: 20150514210618) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20150512191438) do
     t.integer  "user_id"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+    t.boolean  "ready",      default: false
   end
 
   add_index "schedules", ["user_id"], name: "index_schedules_on_user_id", using: :btree
